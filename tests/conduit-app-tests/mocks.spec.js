0a14b697c17d37e7cdaf06381308a0e625d00e10
DanR <dramos.qa@gmail.com>
2024-09-08 17:41:29 -0700

    Refactor test fixtures to use randomize data and utilize a fixture for authentication setup

--- tests/conduit-app-tests/mocks.spec.js
+++ tests/conduit-app-tests/mocks.spec.js
@@ -1,20 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test } from '../../utils/testFixtures';
+import { expect } from '@playwright/test';
 import tagsData from '../../test-data/mocks/tagsData.json';
 import globalFeedArticlesData from '../../test-data/mocks/articles/globalFeedArticlesData.json';
 import yourFeedArticlesData from '../../test-data/mocks/articles/yourFeedArticlesData.json';
-test.use({ storageState: 'playwright/.auth/user.json' });
-test.beforeEach(async ({ page }) => {
-  await page.goto('https://conduit.bondaracademy.com/');
+test.beforeEach(async ({ authenticatedPage }) => {
+  await authenticatedPage.goto('https://conduit.bondaracademy.com/');
 });
-test.skip('Mock Articles Global Feed Page', async ({ page }) => {
-  await page.route('**/api/articles?limit=*&offset=*', async (route) => {
+test('Mock Articles Global Feed Page', async ({ authenticatedPage }) => {
+  await authenticatedPage.route('**/api/articles?limit*', async (route) => {
     await route.fulfill({
       body: JSON.stringify(globalFeedArticlesData),
     });
   });
 
-  await page.locator('.preview-link').last().waitFor();
-  const getArticlesTitle = page.locator('.preview-link h1');
+  await authenticatedPage.locator('.preview-link').last().waitFor();
+  const getArticlesTitle = authenticatedPage.locator('.preview-link h1');
   const getArticlesTitleCount = await getArticlesTitle.count();
 
   for (let i = 0; i < getArticlesTitleCount; i++) {
@@ -22,33 +22,33 @@ test.skip('Mock Articles Global Feed Page', async ({ page }) => {
     expect(articleTitle.includes(globalFeedArticlesData.articles[i].title));
   }
 });
-test.skip('Empty Mock Global Feed Page', async ({ page }) => {
-  await page.route('**/api/articles?limit=*&offset=*', async (route) => {
+test('Empty Mock Global Feed Page', async ({ authenticatedPage }) => {
+  await authenticatedPage.route('**/api/articles?limit*', async (route) => {
     await route.fulfill({
       body: JSON.stringify([]),
     });
   });
 
-  await page.waitForTimeout(1500);
+  await authenticatedPage.waitForTimeout(1500);
 
-  await expect(page.locator('.article-preview')).toHaveText(
+  await expect(authenticatedPage.locator('.article-preview')).toHaveText(
     /no articles are here... yet./i
   );
 });
-test.skip('Mock Articles Your Feed Page', async ({ page }) => {
-  await page.route('**/api/articles/feed?limit=*offset=*', async (route) => {
+test('Mock Articles Your Feed Page', async ({ authenticatedPage }) => {
+  await authenticatedPage.route('**/api/articles/feed?limit*', async (route) => {
     await route.fulfill({
       body: JSON.stringify(yourFeedArticlesData),
     });
   });
-  await page.waitForTimeout(1500);
+  await authenticatedPage.waitForTimeout(1500);
 
-  await page.getByText(/your feed/i).click();
+  await authenticatedPage.getByText(/your feed/i).click();
 
-  await page.waitForTimeout(1500);
+  await authenticatedPage.waitForTimeout(1500);
 
-  await page.locator('.preview-link').last().waitFor();
-  const getArticlesTitle = page.locator('.preview-link h1');
+  await authenticatedPage.locator('.preview-link').last().waitFor();
+  const getArticlesTitle = authenticatedPage.locator('.preview-link h1');
   const getArticlesTitleCount = await getArticlesTitle.count();
 
   for (let i = 0; i < getArticlesTitleCount; i++) {
@@ -56,32 +56,32 @@ test.skip('Mock Articles Your Feed Page', async ({ page }) => {
     expect(articleTitle.includes(yourFeedArticlesData.articles[i].title));
   }
 });
-test.skip('Empty Mock Your Feed Page', async ({ page }) => {
-  await page.route('**/api/articles/feed?limit=*&offset=*', async (route) => {
+test('Empty Mock Your Feed Page', async ({ authenticatedPage }) => {
+  await authenticatedPage.route('**/api/articles/feed?limit*', async (route) => {
     await route.fulfill({
       body: JSON.stringify([]),
     });
   });
 
-  await page.waitForTimeout(1500);
+  await authenticatedPage.waitForTimeout(1500);
 
-  await page.getByText(/your feed/i).click();
+  await authenticatedPage.getByText(/your feed/i).click();
 
-  await page.waitForTimeout(1500);
+  await authenticatedPage.waitForTimeout(1500);
 
-  await expect(page.locator('.article-preview')).toHaveText(
+  await expect(authenticatedPage.locator('.article-preview')).toHaveText(
     /no articles are here... yet./i
   );
 });
-test.skip('Mock tags', async ({ page }) => {
-  await page.route('**/api/tags', async (route) => {
+test('Mock tags', async ({ authenticatedPage }) => {
+  await authenticatedPage.route('**/api/tags', async (route) => {
     await route.fulfill({
       body: JSON.stringify(tagsData),
     });
   });
 
-  await page.locator('.sidebar .tag-list a').last().waitFor();
-  const getSideBarTags = page.locator('.sidebar .tag-list a');
+  await authenticatedPage.locator('.sidebar .tag-list a').last().waitFor();
+  const getSideBarTags = authenticatedPage.locator('.sidebar .tag-list a');
   const getSideBarTagsCount = await getSideBarTags.count();
 
   for (let i = 0; i < getSideBarTagsCount; i++) {

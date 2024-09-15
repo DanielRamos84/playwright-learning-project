37d1baecf71bcd61a25538957347fef9194122f8
DanR <dramos.qa@gmail.com>
2024-09-14 18:32:54 -0700

    Refactor testFixtures.ts to implement Page Object Model for various pages

--- utils/testFixtures.ts
+++ utils/testFixtures.ts
@@ -1,11 +1,28 @@
-import { test as baseTest, expect } from '@playwright/test';
+import { test as baseTest, expect as baseExpect } from '@playwright/test';
 import * as dotenv from 'dotenv';
+import { NavigationPage } from '../page-objects/general/navigationPage';
+import { BrowserWindowsPage } from '../page-objects/alerts-frame-windows/browserWindowsPage';
+import { BookStoreLoginPage } from '../page-objects/book-store-application/bookStoreLoginPage';
+import { CheckboxPage } from '../page-objects/checkBox/checkBoxPage';
+import { TextBoxPage } from '../page-objects/elements/textBoxPage';
+import { PracticeFormPage } from '../page-objects/forms/practiceFormPage';
+import { SortablePage } from '../page-objects/interactions/sortablePage';
+import { AccordianPage } from '../page-objects/widget/accordianPage';
 dotenv.config();
+export const expect = baseExpect;
 
 // Extend Playwright test with fixtures
 export const test = baseTest.extend<{
   authenticatedPage: any;
   accessToken: string;
+  browserWindowsPage: BrowserWindowsPage;
+  bookStoreLoginPage: BookStoreLoginPage;
+  checkBoxPage: CheckboxPage;
+  textBoxPage: TextBoxPage;
+  practiceFormPage: PracticeFormPage;
+  navigationPage: NavigationPage;
+  sortablePage: SortablePage;
+  accordianPage: AccordianPage;
 }>({
   // Define 'accessToken' fixture
   accessToken: async ({ request }, use) => {
@@ -51,5 +68,37 @@ export const test = baseTest.extend<{
 
     // Provide the authenticated page to the test
     await use(page);
+  },
+  // POM BrowserWindowsPage fixture
+  browserWindowsPage: async ({ page }, use) => {
+    await use(new BrowserWindowsPage(page));
+  },
+  // POM BooksStoreLoginPage fixture
+  bookStoreLoginPage: async ({ page }, use) => {
+    await use(new BookStoreLoginPage(page));
+  },
+  // POM CheckboxPage fixture
+  checkBoxPage: async ({ page }, use) => {
+    await use(new CheckboxPage(page));
+  },
+  // POM TextBoxPage fixture
+  textBoxPage: async ({ page }, use) => {
+    await use(new TextBoxPage(page));
+  },
+  // POM PracticeFormPage fixture
+  practiceFormPage: async ({ page }, use) => {
+    await use(new PracticeFormPage(page));
+  },
+  // POM NavigationPage fixture
+  navigationPage: async ({ page }, use) => {
+    await use(new NavigationPage(page));
+  },
+  // POM SortablePage fixture
+  sortablePage: async ({ page }, use) => {
+    await use(new SortablePage(page));
+  },
+  // POM AccordianPage fixture
+  accordianPage: async ({ page }, use) => {
+    await use(new AccordianPage(page));
   },
 });

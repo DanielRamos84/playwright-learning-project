eb59c31bd66a616b813b93ffda73998bd614b2af
DanR <dramos.qa@gmail.com>
2024-09-19 15:51:22 -0700

    Add API fixtures for article creation and deletion in tests

--- tests/conduit-app-tests/working_with_api.spec.ts
+++ tests/conduit-app-tests/working_with_api.spec.ts
@@ -1,141 +1,97 @@
 import { test } from '../../utils/testFixtures';
 import { expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 test.describe.configure({ mode: 'serial' });
-test.beforeAll(async ({ request, accessToken }) => {
-  // Ensure the token is valid and delete any existing articles
-  const userArticlesResponse = await request.get(
-    'https://conduit-api.bondaracademy.com/api/articles?author=Nikita%20Schaefer51',
-    {
-      headers: {
-        Authorization: `Token ${accessToken}`,
-      },
-    }
-  );
-
-  if (userArticlesResponse.status() === 401) {
-    console.log('Token expired or invalid. Triggering re-authentication...');
-    // Reset the accessToken
-    await test.getFixtures().accessToken({ request }, () => {});
-  }
-
-  const userArticles = await userArticlesResponse.json();
-  expect(userArticlesResponse.status()).toEqual(200);
-
-  // Delete any existing articles
-  if (userArticles.articles.length > 0) {
-    for (const article of userArticles.articles) {
-      const deleteResponse = await request.delete(
-        `https://conduit-api.bondaracademy.com/api/articles/${article.slug}`,
-        {
-          headers: {
-            Authorization: `Token ${accessToken}`,
-          },
-        }
-      );
-      if (!deleteResponse.ok()) {
-        console.log(`Failed to delete article: ${article.slug}`);
-      } else {
-        console.log(`Found and deleted article: ${article.slug}`);
-      }
-    }
-  } else {
-    console.log('No articles found for the user.');
-  }
+test.beforeAll(async ({ accessToken, deleteArticlesApi }) => {
+  // Use the deleteArticlesApi fixture to delete the articles with the accessToken
+  await deleteArticlesApi(accessToken);
 });
-test('Creates Article', async ({ request, accessToken, authenticatedPage }) => {
-  // Create new article via API
-  const postNewArticleResponse = await request.post(
-    'https://conduit-api.bondaracademy.com/api/articles/',
-    {
-      data: {
-        article: {
-          title: 'Dynamic Functionality Consultant',
-          description:
-            'Aliquid vitae pariatur nam dignissimos est tempore quam qui aspernatur.',
-          body: 'Soluta consectetur esse aliquid nostrum neque maxime.',
-          tagList: ['Ea maiores quo veritatis laborum.'],
-        },
-      },
-      headers: {
-        Authorization: `Token ${accessToken}`,
-      },
-    }
-  );
+test('Creates Article', async ({
+  accessToken,
+  authenticatedPage,
+  createArticleApi,
+}) => {
+  const fakeTitle = faker.company.catchPhrase();
+  const fakeDescription = faker.lorem.sentence();
+  const fakeBody = faker.lorem.paragraph();
+  const fakeTagList = [faker.hacker.noun()];
 
-  expect(postNewArticleResponse.status()).toEqual(201);
+  // Use the createArticleApi fixture to create an article with the accessToken
+  await createArticleApi(accessToken, {
+    title: fakeTitle,
+    description: fakeDescription,
+    body: fakeBody,
+    tagList: fakeTagList,
+  });
 
   // Assert article is visible from home authenticatedPage global feed tab
-  await authenticatedPage.goto('https://conduit.bondaracademy.com', { waitUntil: 'commit' });
-  await authenticatedPage.locator('.article-preview').last().waitFor();
+  await authenticatedPage.goto('https://conduit.bondaracademy.com');
+
+  await authenticatedPage
+    .locator('.article-preview')
+    .last()
+    .waitFor({ state: 'visible' });
+
   await expect(
     authenticatedPage.getByRole('heading', {
-      name: /dynamic functionality consultant/i,
+      name: fakeTitle,
     })
   ).toBeVisible();
 
   // Assert article is visible from profile authenticatedPage
   await authenticatedPage.goto(
-    'https://conduit.bondaracademy.com/profile/Nikita%20Schaefer51', { waitUntil: 'commit' }
+    'https://conduit.bondaracademy.com/profile/Nikita%20Schaefer51'
   );
-  await authenticatedPage.locator('.article-preview').last().waitFor();
+
+  await authenticatedPage
+    .locator('.article-preview')
+    .waitFor({ state: 'visible' });
+
   await expect(
     authenticatedPage.getByRole('heading', {
-      name: /dynamic functionality consultant/i,
+      name: fakeTitle,
     })
   ).toBeVisible();
 });
-test('Delete Article', async ({ request, authenticatedPage, accessToken }) => {
-  // create new article via API
-  const postNewArticleResponse = await request.post(
-    'https://conduit-api.bondaracademy.com/api/articles/',
-    {
-      data: {
-        article: {
-          title: 'Direct Applications Manager',
-          description:
-            'Aliquam quos eaque aliquid autem quam nisi vero suscipit consectetur.',
-          body: 'Aliquam quos eaque aliquid autem quam nisi vero suscipit consectetur.',
-          tagList: [
-            'Et consectetur ratione ratione quidem error culpa quae ducimus fuga.',
-          ],
-        },
-      },
-      headers: {
-        Authorization: `Token ${accessToken}`,
-      },
-    }
-  );
+test('Delete Article', async ({
+  request,
+  authenticatedPage,
+  accessToken,
+  deleteArticlesApi,
+  createArticleApi,
+}) => {
+  const fakeTitle = faker.company.catchPhrase();
+  const fakeDescription = faker.lorem.sentence();
+  const fakeBody = faker.lorem.paragraph();
+  const fakeTagList = [faker.hacker.noun()];
 
-  expect(postNewArticleResponse.status()).toEqual(201);
-  const postNewArticleResponseData = await postNewArticleResponse.json();
+  // Use the createArticleApi fixture to create an article with the accessToken
+  const postNewArticleResponseData = await createArticleApi(accessToken, {
+    title: fakeTitle,
+    description: fakeDescription,
+    body: fakeBody,
+    tagList: fakeTagList,
+  });
 
-  //delete article through api
-  const deleteArticleResponse = await request.delete(
-    `https://conduit-api.bondaracademy.com/api/articles/${postNewArticleResponseData.article.slug}`,
-    {
-      headers: {
-        Authorization: `Token ${accessToken}`,
-      },
-    }
-  );
+  const slug = postNewArticleResponseData.article.slug;
 
-  expect(deleteArticleResponse.status()).toEqual(204);
+  await deleteArticlesApi(accessToken);
 
   //assert article is not found from home authenticatedPage
-  await authenticatedPage.goto('https://conduit.bondaracademy.com', { waitUntil: 'commit' });
+  await authenticatedPage.goto('https://conduit.bondaracademy.com');
   await expect(
     authenticatedPage.getByRole('heading', {
-      hasText: postNewArticleResponseData.article.title,
+      hasText: fakeTitle,
     })
   ).not.toBeVisible();
 
   //assert article is not found from profile authenticatedPage
   await authenticatedPage.goto(
-    'https://conduit.bondaracademy.com/profile/Nikita%20Schaefer51', { waitUntil: 'commit' }
+    'https://conduit.bondaracademy.com/profile/Nikita%20Schaefer51'
   );
   await expect(
     authenticatedPage.getByRole('heading', {
-      hasText: postNewArticleResponseData.article.title,
+      hasText: fakeTitle,
     })
   ).not.toBeVisible();
 });
@@ -143,43 +99,40 @@ test('Mark Article as Favorite', async ({
   request,
   accessToken,
   authenticatedPage,
+  createArticleApi,
 }) => {
-  // create new article via API
-  const postNewArticleResponse = await request.post(
-    'https://conduit-api.bondaracademy.com/api/articles/',
-    {
-      data: {
-        article: {
-          title: 'Future Web Representative',
-          description: 'Ratione alias esse.',
-          body: 'Quasi ad ea natus.',
-          tagList: [
-            'Nemo molestiae mollitia quisquam possimus dolor accusamus.',
-          ],
-        },
-      },
-      headers: {
-        Authorization: `Token ${accessToken}`,
-      },
-    }
-  );
+  const fakeTitle = faker.company.catchPhrase();
+  const fakeDescription = faker.lorem.sentence();
+  const fakeBody = faker.lorem.paragraph();
+  const fakeTagList = [faker.hacker.noun()];
 
-  expect(postNewArticleResponse.status()).toEqual(201);
-  const postNewArticleResponseData = await postNewArticleResponse.json();
+  // Use the createArticleApi fixture to create an article with the accessToken
+  const postNewArticleResponseData = await createArticleApi(accessToken, {
+    title: fakeTitle,
+    description: fakeDescription,
+    body: fakeBody,
+    tagList: fakeTagList,
+  });
+
+  const slug = postNewArticleResponseData.article.slug;
 
   // assert article is not marked as favorite from home global feed
-  await authenticatedPage.goto('https://conduit.bondaracademy.com', { waitUntil: 'commit' });
-  await authenticatedPage.locator('.article-preview').last().waitFor();
-  const articleContainer = authenticatedPage.locator('.article-preview', {
+  await authenticatedPage.goto('https://conduit.bondaracademy.com');
+  await authenticatedPage
+    .locator('.article-preview')
+    .last()
+    .waitFor({ state: 'visible' });
+  
+    const articleContainer = authenticatedPage.locator('.article-preview', {
     has: authenticatedPage.locator('h1', {
-      hasText: postNewArticleResponseData.article.title,
+      hasText: fakeTitle,
     }),
   });
   await expect(articleContainer.locator('.pull-xs-right')).toHaveText('0');
 
   // mark article as favorite via API
   const favoriteArticleResponse = await request.post(
-    `https://conduit-api.bondaracademy.com/api/articles/${postNewArticleResponseData.article.slug}/favorite`,
+    `https://conduit-api.bondaracademy.com/api/articles/${slug}/favorite`,
     {
       data: {
         article: {
@@ -196,51 +149,48 @@ test('Mark Article as Favorite', async ({
   expect(favoriteArticleResponse.status()).toEqual(200);
 
   // assert article is marked as favorite from home authenticatedPage
-  await authenticatedPage.goto('https://conduit.bondaracademy.com', { waitUntil: 'commit' });
-  await authenticatedPage.locator('.article-preview').last().waitFor();
+  await authenticatedPage.goto('https://conduit.bondaracademy.com');
+  await authenticatedPage
+    .locator('.article-preview')
+    .last()
+    .waitFor({ state: 'visible' });
   await expect(articleContainer.locator('.pull-xs-right')).toHaveText('1');
 
   // assert article is marked as favorite from profile authenticatedPage
   await authenticatedPage.goto(
-    'https://conduit.bondaracademy.com/profile/Nikita%20Schaefer51', { waitUntil: 'commit' }
+    'https://conduit.bondaracademy.com/profile/Nikita%20Schaefer51'
   );
-  await authenticatedPage.locator('.article-preview').last().waitFor();
+  await authenticatedPage
+    .locator('.article-preview')
+    .last()
+    .waitFor({ state: 'visible' });
   await expect(articleContainer.locator('.pull-xs-right')).toHaveText('1');
 });
 test('Post Comment on Personal Article', async ({
   request,
   accessToken,
   authenticatedPage,
+  createArticleApi,
 }) => {
-  // create new article via API
-  const postNewArticleResponse = await request.post(
-    'https://conduit-api.bondaracademy.com/api/articles/',
-    {
-      data: {
-        article: {
-          title: 'National Division Officer',
-          description:
-            'Ipsum accusamus debitis deleniti adipisci distinctio totam voluptatem ad ratione.',
-          body: 'Illo voluptatem illum a ex vero nostrum tempore culpa.',
-          tagList: [
-            'Tempora consectetur est doloribus laudantium dolor repudiandae in labore.',
-          ],
-        },
-      },
-      headers: {
-        Authorization: `Token ${accessToken}`,
-      },
-    }
-  );
+  const fakeTitle = faker.company.catchPhrase();
+  const fakeDescription = faker.lorem.sentence();
+  const fakeBody = faker.lorem.paragraph();
+  const fakeTagList = [faker.hacker.noun()];
 
-  expect(postNewArticleResponse.status()).toEqual(201);
-  const postNewArticleResponseJson = await postNewArticleResponse.json();
-  const articleSlug = postNewArticleResponseJson.article.slug;
-  const userName = postNewArticleResponseJson.article.author.username;
+  // Use the createArticleApi fixture to create an article with the accessToken
+  const postNewArticleResponseData = await createArticleApi(accessToken, {
+    title: fakeTitle,
+    description: fakeDescription,
+    body: fakeBody,
+    tagList: fakeTagList,
+  });
+
+  const slug = postNewArticleResponseData.article.slug;
+  const userName = postNewArticleResponseData.article.author.username;
 
   // post comment via API
   const commentOwnArticleResponse = await request.post(
-    `https://conduit-api.bondaracademy.com/api/articles/${articleSlug}/comments`,
+    `https://conduit-api.bondaracademy.com/api/articles/${slug}/comments`,
     {
       data: {
         comment: {
@@ -258,11 +208,13 @@ test('Post Comment on Personal Article', async ({
   const comment = commentJson.comment.body;
 
   await authenticatedPage.goto(
-    `https://conduit.bondaracademy.com/article/${articleSlug}`, { waitUntil: 'commit' }
+    `https://conduit.bondaracademy.com/article/${slug}`
   );
 
   // iterate over card containers in case there's more than one find the matching one
-  await authenticatedPage.locator('[class="card"]').waitFor();
+  await authenticatedPage
+    .locator('[class="card"]')
+    .waitFor({ state: 'visible' });
   const commentCards = authenticatedPage.locator('[class="card"]');
   const commentCardsCount = await commentCards.count();
 
--- utils/testFixtures.ts
+++ utils/testFixtures.ts
@@ -1,14 +1,19 @@
 import { test as baseTest, expect as baseExpect } from '@playwright/test';
 import { PageObjectManager } from './pageObjectManager';
 import * as dotenv from 'dotenv';
+import { DeleteArticlesApi, CreateArticleApi } from '../types/fixtures';
+import { ArticleData, ArticleResponse } from '../types/article';
+
 dotenv.config();
+
 export const expect = baseExpect;
 
-// Extend Playwright test with fixtures
 export const test = baseTest.extend<{
   authenticatedPage: any;
   accessToken: string;
   pageObjectManager: PageObjectManager;
+  deleteArticlesApi: (accessToken: string) => Promise<void>;
+  createArticleApi: CreateArticleApi;
 }>({
   // Define 'accessToken' fixture
   accessToken: async ({ request }, use) => {
@@ -40,13 +45,7 @@ export const test = baseTest.extend<{
     // Provide the token to the test
     await use(token);
   },
-  // POM PageObjectManager fixture
-  pageObjectManager: async ({ page }, use) => {
-    // const pm = new PageObjectManager(page);
-    // await use(pm);
 
-    await use(new PageObjectManager(page));
-  },
   // Define 'authenticatedPage' fixture
   authenticatedPage: async ({ page, accessToken }, use) => {
     if (accessToken) {
@@ -60,5 +59,78 @@ export const test = baseTest.extend<{
 
     // Provide the authenticated page to the test
     await use(page);
+  },
+
+  // POM PageObjectManager fixture
+  pageObjectManager: async ({ page }, use) => {
+    await use(new PageObjectManager(page));
+  },
+
+  // API Deletes All Articles fixture
+  deleteArticlesApi: async ({ request }, use) => {
+    const deleteArticles: DeleteArticlesApi = async (accessToken: string) => {
+      const userArticlesResponse = await request.get(
+        'https://conduit-api.bondaracademy.com/api/articles?author=Nikita%20Schaefer51',
+        {
+          headers: {
+            Authorization: `Token ${accessToken}`,
+          },
+        }
+      );
+
+      if (userArticlesResponse.status() !== 200) {
+        throw new Error('Failed to fetch user articles');
+      }
+
+      const userArticles = await userArticlesResponse.json();
+
+      if (userArticles.articles.length > 0) {
+        for (const article of userArticles.articles) {
+          const deleteResponse = await request.delete(
+            `https://conduit-api.bondaracademy.com/api/articles/${article.slug}`,
+            {
+              headers: {
+                Authorization: `Token ${accessToken}`,
+              },
+            }
+          );
+          if (!deleteResponse.ok()) {
+            console.error(`Failed to delete article: ${article.slug}`);
+          } else {
+            console.log(`Deleted article: ${article.slug}`);
+          }
+        }
+      } else {
+        console.log('No articles found for the user.');
+      }
+    };
+
+    // Use the fixture in tests
+    await use(deleteArticles);
+  },
+
+  // API Creates Article fixture
+  createArticleApi: async ({ request }, use) => {
+    const createArticle: CreateArticleApi = async (
+      accessToken: string,
+      articleData: ArticleData
+    ): Promise<ArticleResponse> => {
+      const postNewArticleResponse = await request.post(
+        'https://conduit-api.bondaracademy.com/api/articles/',
+        {
+          data: {
+            article: articleData,
+          },
+          headers: {
+            Authorization: `Token ${accessToken}`,
+          },
+        }
+      );
+
+      expect(postNewArticleResponse.status()).toEqual(201);
+      return postNewArticleResponse.json() as Promise<ArticleResponse>;
+    };
+
+    await use(createArticle);
   },
 });
